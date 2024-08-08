873f17e060e68b4e09389a8ed2707859e2df841d
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-08-08 15:17:29 -0300

    Criando Sidebar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,17 @@
-import { Logo } from "@/components/logo"
-import { ModeToggle } from "@/components/mode-toggle"
+import { Logo } from "@/components/logo";
+import { ModeToggle } from "@/components/mode-toggle";
+import { CartSideBar } from "@/components/cart/sidebar";
 
 export const Header = () => {
-    return (
-        <header className="flex justify-between items-center my-5 mx-3">
-            <div className="flex items-center gap-3">
-                <Logo />
-                <ModeToggle />
-            </div>
-            <div className="flex items-center gap-3">
-                ...
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="flex justify-between items-center my-5 mx-3">
+      <div className="flex items-center gap-3">
+        <Logo />
+        <ModeToggle />
+      </div>
+      <div className="flex items-center gap-3">
+        <CartSideBar />
+      </div>
+    </header>
+  );
+};
