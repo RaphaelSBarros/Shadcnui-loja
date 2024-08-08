f4ab86a3ab776bee756a825d2d1f1e140c8a8ef7
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-08-08 16:07:00 -0300

    Criando Tab de Produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { ModeToggle } from "@/components/mode-toggle"
+import { Footer } from "@/components/footer";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Header } from "@/components/header";
+import { Suspense } from "react";
+import { ProductsTab } from "@/components/products/tab";
+import { TabsSkeleton } from "@/components/products/skeleton";
 
 const Page = () => {
   return (
-    <div>
-      <ModeToggle />
+    <div className="w-full max-w-4xl mx-auto">
+      <Header />
+      <div className="mx-3">
+        <Suspense fallback={<TabsSkeleton />}>
+          <ProductsTab />
+        </Suspense>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
