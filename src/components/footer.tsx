ab61154e77fed151d5614e6fecbe912f5f54232d
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-08-12 11:43:56 -0300

    Adicionando margin-top no componente

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { Separator } from "@/components/ui/separator";
 
 export const Footer = () => {
 	return (
-		<footer>
+		<footer className="mt-5">
 			<Separator />
 			<div className="my-5 text-center text-sm opacity-50">
 				Criado por Raphael
